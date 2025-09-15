4ea4fb018f49954df277a8f5bca2d1094a6f3c00
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-16 00:54:34 +0300

    Update NFTGrid.js

--- components/NFTGrid.js
+++ components/NFTGrid.js
@@ -1,25 +1,20 @@
 'use client';
-
 import NFTCard from './NFTCard';
 import { useState } from 'react';
 import NFTDetailModal from './NFTDetailModal';
 
-export default function NFTGrid({ nfts }) {
+export default function NFTGrid({ nfts = [] }) {
   const [selectedNFT, setSelectedNFT] = useState(null);
-
-  const handleClose = () => setSelectedNFT(null);
 
   return (
     <div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
         {nfts.map((nft) => (
           <NFTCard key={nft.id} nft={nft} onClick={() => setSelectedNFT(nft)} />
         ))}
       </div>
 
-      {selectedNFT && (
-        <NFTDetailModal nft={selectedNFT} onClose={handleClose} />
-      )}
+      {selectedNFT && <NFTDetailModal nft={selectedNFT} onClose={() => setSelectedNFT(null)} />}
     </div>
   );
 }
