3f484ec0b871a22a0a2cbff557bd7f822b11f54d
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-16 00:37:15 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,35 @@
 'use client';
+
+
 import NFTGrid from '../components/NFTGrid';
-export default function Home() {
-return (
-<main className="p-6">
-<h1 className="text-3xl font-bold mb-6">Anoma NFT Marketplace Demo</h1>
-<NFTGrid />
-</main>
-);
+
+
+// Demo NFT verisi
+const demoNFTs = [
+{
+id: 1,
+name: 'Anoma NFT #1',
+owner: '0x123...',
+price: '0.5 ETH',
+image: '/images/nft1.png'
+},
+{
+id: 2,
+name: 'Anoma NFT #2',
+owner: '0xabc...',
+price: '1.2 ETH',
+image: '/images/nft2.png'
+},
+{
+id: 3,
+name: 'Anoma NFT #3',
+owner: '0xdef...',
+price: '0.8 ETH',
+image: '/images/nft3.png'
+}
+];
+
+
+export default function Page() {
+return <NFTGrid nfts={demoNFTs} />;
 }
