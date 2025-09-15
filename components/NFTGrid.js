6e005c0a8d2d39eb4836a4625cb1a6af66812708
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-16 01:30:49 +0300

    Update NFTGrid.js

--- components/NFTGrid.js
+++ components/NFTGrid.js
@@ -1,5 +1,6 @@
 'use client';
 import NFTCard from './NFTCard';
+
 export default function NFTGrid({ nfts }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
